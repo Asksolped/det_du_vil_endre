67444054bd125aa800b6b8932590bff6db0c3289
johnb08 <johnb08.kodehode@gmail.com>
2025-08-20 13:26:29 +0200

    added changes to file

--- fillFields.js
+++ fillFields.js
@@ -1,16 +1,12 @@
 import createWeatherImage from "./createWeatherImage.js"
 
 const fillFields = async (fields, url) =>{
-    console.log(fields);
     const response = await fetch(url);
     const result = await response.json();
-    console.log(result);
     const data = result.properties.timeseries[0].data.instant.details;
     const imgTag = result.properties.timeseries[0].data.next_1_hours.summary.symbol_code;
     fields.forEach((field) => {
         const value = field.querySelector(".value");
-        console.log(value);
-        console.log(value.children);
         switch (value.id) {
             case "airTemp":
                 value.textContent = data.air_temperature;
